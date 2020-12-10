bf50b8e7c082e9e6b1582ce623bbc3797a6418da
Kosmas Raptis <keeperkosmas6@gmail.com>
2020-12-11 00:27:30 +0200

    Add basic CPU.h/.c and common.h

---
+++ cpu.h
@@ -0,0 +1,11 @@
+//
+// Created by kosmas on 11/12/20.
+//
+
+#ifndef CHIP8EMU_CPU_H
+#define CHIP8EMU_CPU_H
+
+#include "common.h"
+
+
+#endif //CHIP8EMU_CPU_H
--- memory.h
+++ memory.h
@@ -5,20 +5,7 @@
 #ifndef CHIP8EMU_MEMORY_H
 #define CHIP8EMU_MEMORY_H
 
-typedef unsigned char byte;
-
-typedef union {
-    unsigned short int WORD;
-    struct {
-#ifdef BIG_ENDIAN
-        //On Big Endian architectures, the bytes are stored in the order of high to low
-          byte high, low;
-#else
-        //On Little Endian architectures, the bytes are stored in the order of low to high
-        byte low, high;
-#endif
-    }BYTE;
-}word;
+#include "common.h"
 
 byte *memory;
 
