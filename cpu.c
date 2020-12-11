9c5c0e532b2272028ef2d4a742baba494c3b518e
Kosmas Raptis <keeperkosmas6@gmail.com>
2020-12-11 08:39:39 +0200

    Move CPU register set to header file instead of source file

--- cpu.c
+++ cpu.c
@@ -3,15 +3,3 @@
 //
 
 #include "cpu.h"
-
-typedef struct {
-    byte registers[16];
-    word indexreg;
-    word spointer;
-    byte stimer;
-    byte dtimer;
-    word pcounter;
-    word operand;
-}chip8regs;
-
-chip8regs cpu;
--- cpu.h
+++ cpu.h
@@ -7,5 +7,16 @@
 
 #include "common.h"
 
+typedef struct {
+    byte registers[16];
+    word indexreg;
+    word spointer;
+    byte stimer;
+    byte dtimer;
+    word pcounter;
+    word operand;
+}chip8regs;
+
+chip8regs cpu;
 
 #endif //CHIP8EMU_CPU_H
