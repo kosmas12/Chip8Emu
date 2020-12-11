c18706c8f037a09658685a0b73cb268c3bc19e69
Kosmas Raptis <keeperkosmas6@gmail.com>
2020-12-11 08:38:53 +0200

    Finish writing CPU register set

--- cpu.c
+++ cpu.c
@@ -4,6 +4,14 @@
 
 #include "cpu.h"
 
-byte registers[16];
-word indexreg;
-word stackpointer;
+typedef struct {
+    byte registers[16];
+    word indexreg;
+    word spointer;
+    byte stimer;
+    byte dtimer;
+    word pcounter;
+    word operand;
+}chip8regs;
+
+chip8regs cpu;
